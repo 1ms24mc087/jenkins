f16278131aabe67d96846c9b634e063ab908126d
1ms24mc087 <1ms24mc087@msrit.edu>
2025-11-12 21:51:33 +0530

    Create numbers.java

---
+++ numbers.java
@@ -0,0 +1,12 @@
+class numbers
+  {
+    public static void main(String[] args)
+    {
+      int i=1;
+      System.out.println("Printing numbers from 1-10");
+      for(i=1;i<=10;i++)
+        {
+          System.out.println(i);
+        }
+    }
+  }
